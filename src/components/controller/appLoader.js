cd3ea415dc6b28851e402cf0a9d4e42f36747202
sanches311 <itchipset@gmail.com>
2022-12-04 19:38:36 +0300

    init: start migration-newip-to-ts task

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '23b0959296e8471abc8afbccdf74b223', // получите свой ключ https://newsapi.org/
         });
     }
 }
